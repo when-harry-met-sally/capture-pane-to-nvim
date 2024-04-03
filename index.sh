c11d4ca3b84b7112a6f7b34373a5a060ff12ccb8
Miles <miles@karmacheck.com>
2024-04-03 15:59:35 -0500

    using bash now

---
+++ index.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+pane_id=$1
+tempfile=$(mktemp)
+
+tmux capture-pane -pS -2000 -t "$pane_id" | sed 's/[[:space:]]*$//' > "$tempfile"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e :a -e '/^\n*$/{$d;N;};/\n$/ba' "$tempfile"
+else
+  sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' "$tempfile"
+fi
+
+nvim -n + "$tempfile" && rm "$tempfile"
